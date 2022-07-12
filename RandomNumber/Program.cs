9b4e93324866436096c98dac5c20f8ae8010693e
jedrzej.szyszka <jedrzej.szyszka@WRO1-LDL-P90337>
2022-07-12 09:54:33 +0200

    Add project files.

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,6 @@
+﻿Random rnd = new Random();
+int num = rnd.Next(0, 100);
+Console.WriteLine(num);
+String str = Convert.ToString(num);
+File.WriteAllText(@"C:\Users\jedrzej.szyszka\Desktop\Results\randomnumber.txt", str);
+
---
+++ RandomNumberTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace RandomNumberTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
